6f1b88d771da03d9841efc9728369805ac9dd275
Waramporn Yangkijkarn <58160637@go.buu.ac.th>
2017-09-02 12:11:08 +0700

    add test echo 'Fifteen - Love' when player A get score.

--- tennis.spec.js
+++ tennis.spec.js
@@ -1,10 +1,18 @@
-function TennisGame(){
-  this.reset = () => {
+function TennisGame() {
+    const scoreString = ['Love', 'Fifteen', 'Thirty', 'Forthy']
+    this.playerAScore = 0
 
-  }
-  this.echo = () => {
-    return 'Love - Love'
-  }
+    this.reset = () => {
+        this.playerAScore = 0
+    }
+
+    this.echo = () => {
+       return `${scoreString[this.playerAScore]} - Love`
+    }
+
+    this.playerAGteScore = () => {
+        this.playerAScore++
+    }
 }
 
 test('Echo "Love - Love" when game start', () => {
@@ -15,4 +23,17 @@ test('Echo "Love - Love" when game start', () => {
   let result = app.echo()
   //assert
   expect(result).toBe('Love - Love')
+})
+
+test('Echo "Fifteen - Love" when playerA get first score', () => {
+    // Arrange
+    let app = new TennisGame()
+    app.reset()
+    app.playerAGteScore()
+
+    // Act
+    let result = app.echo()
+
+    // Assert
+    expect(result).toBe('Fifteen - Love')
 })
